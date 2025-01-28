efb3328674dff54fcc154ea5956974a39203b598
InaneInaccompli <92868301+InaneInaccompli@users.noreply.github.com>
2025-01-28 09:32:37 +0100

    Service 'Dungeon' fonctionnel

--- services/dungeon/server.js
+++ services/dungeon/server.js
@@ -1,10 +1,10 @@
 import express from 'express';
+import dungeonRoutes from './dungeonRoutes.js'
 const app = express();
 const PORT = 5002;
-
-app.get('/', (req, res) => {
-  res.send('Hello from Dungeon service!');
-});
+// Middleware JSON
+app.use(express.json());
+app.use('/', dungeonRoutes);
 
 app.listen(PORT, () => {
   console.log(`Dungeon service running on port ${PORT}`);
