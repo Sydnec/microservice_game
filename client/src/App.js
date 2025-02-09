0e2bd5cf9149a5e2bd449fb2f76d09b764b1a0f4
InaneInaccompli <92868301+InaneInaccompli@users.noreply.github.com>
2025-02-10 00:09:59 +0100

    Commit final

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 // src/App.js
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Dungeon from './components/Dungeon'
+import Dungeon from './components/Dungeon';
+import Fight from './components/Fight';
 import HeroList from './components/HeroList';
 import HeroCreation from './components/HeroCreation';
 import Game from './components/Game';
@@ -14,6 +15,7 @@ function App() {
         <Route path="/create" element={<HeroCreation />} />
         <Route path="/game" element={<Game />} />
         <Route path="/dungeon" element={<Dungeon />} />
+        <Route path="/fight" element={<Fight />} />
       </Routes>
     </BrowserRouter>
   );
